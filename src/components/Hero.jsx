21bb8ddf9bfb3856642fcf39a6c996b40c69e855
andaru akbar <andaruakbar11@gmail.com>
2022-08-04 02:14:41 +0700

    fixed styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   return (
     <div name="hero" className="w-full h-screen bg-[#0a192f]">
       <div className="max-w-[1000px] grid sm:grid-cols-2 mx-auto px-8 justify-center">
-        <div className="text-center sm:self-center lg:text-right lg:self-center">
-          <p className="mt-20 md:mt-40 text-[#9121f3]">Hi, My Name is</p>
+        <div className="text-center sm:self-center sm:text-right">
+          <p className="mt-20 md:mt-0 text-[#9121f3]">Hi, My Name is</p>
           <h1 className=" text-3xl sm:text-5xl  font-bold text-[#ccd6f6] mb-1">
             Andaru Akbar Priyatmaji
           </h1>
@@ -14,7 +14,7 @@ const Hero = () => {
             Frontend Developer || Web Design
           </h2>
         </div>
-        <div className="pt-10 md:mt-40 flex items-center justify-center">
+        <div className="pt-10 md:mt-0 flex items-center justify-center">
           <img src={andaru} alt="" className="w-60 h-70" />
         </div>
       </div>
